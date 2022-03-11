f8cfd6bdc15b78fb19d300755bddd72864c5c96a
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-03-11 02:49:33 +0000

    i added some words about philza minecraft
    by the way did you know that philza streams mostly minecraft every mondays wednesdays and fridays and occasional saturdays at twitch.tv/philza?

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Created by: Katie
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'watch philza minecraft play (mainly) minecraft (shocker honestly who would have guessed mr minecraft plays minecraft) every monday wednesday and friday, and occasionally saturdays, live at twitch.tv/philza'
+}
